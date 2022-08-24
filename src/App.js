0a7b054563443d2dd7777f7b720efc7c0cef7ecd
Jamie Moore <jamiemoore2689@gmail.com>
2022-08-24 07:50:41 +0100

    small tweaks plus readme update

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
     },[])
 
     return (<>
-        <div className="App">
+        <div className="app">
             {!fetchedData && <Loading /> }
             {fetchedData && <Home characters={charList} /> }
         </div>
